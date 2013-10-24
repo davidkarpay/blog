b01baed4658f87277c3f69360d6fb716d0e1730d
David Karpay <dlkarpay@gmail.com>
2013-10-24 17:41:50 -0400

    added an index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,18 +3,21 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = Post.new(params[:post].permit(:title, :text))
 
 		@post.save
 		redirect_to @post
 	end
 
-	private
 	def post_params
 		params.require(:post).permit(:title, :text)
 	end
 
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def index
+		@posts = Post.all
 	end
 end
