a014d3dcbb690ec73863e6fda8f271becbe73645
David Karpay <dlkarpay@gmail.com>
2013-10-28 14:51:54 -0400

    added an update action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,10 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def update
+		@post = Post.find(params[:id])
+	end
+
 	def post_params
 		params.require(:post).permit(:title, :text)
 	end
